85e596abab4833ec38d37820de29a3f3e92856d0
Shani Bider <shanibider@gmail.com>
2024-04-22 22:24:13 +0300

    add code

--- code/02 JS Refresher/code/finished/assets/scripts/app.js
+++ code/02 JS Refresher/code/finished/assets/scripts/app.js
@@ -198,10 +198,7 @@ setTimeout(() => {
 
 
 
-
-
-
-
+// Functions inside of functions
 function init() {
   function greet() {
     console.log(“Hi!“);
@@ -212,9 +209,250 @@ function init() {
 
 init();
 
+
+
+// reference vs premative values
 const message = “Hello”;
 
 const hobbies = ["Sports", "Cooking"];
 hobbies = [];
 hobbies.push("Working");
 console.log(hobbies);
+
+
+
+// let
+// The let declaration declares re-assignable, block-scoped local variables, optionally initializing each to a value.
+let x = 1;
+
+if (x === 1) {
+  let x = 2;
+  console.log(x);  // Expected output: 2
+}
+console.log(x);// Expected output: 1
+
+
+
+// const
+// The const declaration declares block-scoped local variables.
+// The value of a constant can't be changed through reassignment using the assignment operator,
+// but if a constant is an object, its properties can be added, updated, or removed.
+const number = 42;
+
+try {
+  number = 99;
+} catch (err) {
+  console.log(err);
+  // Expected output: TypeError: invalid assignment to const 'number'
+  // (Note: the exact output may be browser-dependent)
+}
+console.log(number);// Expected output: 42
+
+
+// ES6 Arrow Functions
+// Arrow functions are a different way of creating functions in JavaScript. Besides a shorter syntax, they offer advantages when it comes to keeping the scope of the this  keyword (see here).
+// Arrow function syntax may look strange but it's actually simple.
+
+function callMe(name) { 
+    console.log(name);
+}
+
+// which you could also write as:
+const callMe = function(name) { 
+    console.log(name);
+}
+
+// becomes: 
+const callMe = (name) => { 
+    console.log(name);
+}
+
+
+// Important: 
+// When having no arguments, you have to use empty parentheses in the function declaration:
+const callMe = () => { 
+  console.log('Max!');
+}
+
+// When having exactly one argument, you may omit the parentheses:
+const callMe = name => { 
+  console.log(name);
+}
+
+// When just returning a value, you can use the following shortcut:
+const returnMe = name => name
+
+// That's equal to:
+const returnMe = name => { 
+  return name;
+}
+
+
+
+
+
+
+
+
+// Exports & Imports
+// In React projects (and actually in all modern JavaScript projects),
+// you split your code across multiple JavaScript files - so-called modules.
+// You do this, to keep each file/ module focused and manageable.
+// To still access functionality in another file, you need export  (to make it available) and import  (to get access) statements.
+
+// You got two different types of exports: default (unnamed) and named exports:
+
+default => export default ...; 
+
+named => export const someData = ...; 
+
+// You can import default exports like this:
+import someNameOfYourChoice from './path/to/file.js'; 
+
+// Surprisingly, someNameOfYourChoice  is totally up to you.
+
+// Named exports have to be imported by their name:
+import { someData } from './path/to/file.js'; 
+
+// A file can only contain one default and an unlimited amount of named exports.
+// You can also mix the one default with any amount of named exports in one and the same file.
+// When importing named exports, you can also import all named exports at once with the following syntax:
+import * as upToYou from './path/to/file.js'; 
+
+// `upToYou`  is - well - up to you and simply bundles all exported variables/functions in one JavaScript object.
+// For example, if you export const someData = ...  (/path/to/file.js ) you can access it on upToYou  like this: upToYou.someData .
+
+
+
+
+// Classes
+// Classes are a feature which basically replace constructor functions and prototypes. You can define blueprints for JavaScript objects with them. 
+
+// Like this:
+class Person {
+    constructor () {
+        this.name = 'Max';
+    }
+}
+ 
+const person = new Person();
+console.log(person.name); // prints 'Max'
+
+/* In the above example, not only the class but also a property of that class (=> name ) is defined. The syntax you see there, is the "old" syntax for defining properties. In modern JavaScript projects (as the one used in this course), you can use the following, more convenient way of defining class properties: */
+class Person {
+    name = 'Max';
+}
+ 
+const person = new Person();
+console.log(person.name); // prints 'Max'
+
+// You can also define methods. Either like this:
+class Person {
+    name = 'Max';
+    printMyName () {
+        console.log(this.name); // this is required to refer to the class!
+    }
+}
+ 
+const person = new Person();
+person.printMyName();
+
+// Or like this:
+class Person {
+    name = 'Max';
+    printMyName = () => {
+        console.log(this.name);
+    }
+}
+ 
+const person = new Person();
+person.printMyName();
+
+// The second approach has the same advantage as all arrow functions have: The this  keyword doesn't change its reference.
+
+// You can also use inheritance when using classes:
+class Human {
+    species = 'human';
+}
+ 
+class Person extends Human {
+    name = 'Max';
+    printMyName = () => {
+        console.log(this.name);
+    }
+}
+ 
+const person = new Person();
+person.printMyName();
+console.log(person.species); // prints 'human'
+
+
+
+// Spread & Rest Operator
+// The spread and rest operators actually use the same syntax: `... `
+// It's usage determines whether you're using it as the spread or rest operator.
+
+// Using the Spread Operator:
+// The spread operator allows you to pull elements out of an array (=> split the array into a list of its elements) or pull the properties out of an object. Here are two examples:
+
+const oldArray = [1, 2, 3];
+const newArray = [...oldArray, 4, 5]; // This now is [1, 2, 3, 4, 5];
+
+// Here's the spread operator used on an object:
+const oldObject = {
+    name: 'Max'
+};
+const newObject = {
+    ...oldObject,
+    age: 28
+};
+
+// newObject  would then be
+{
+    name: 'Max',
+    age: 28
+}
+
+/* The spread operator is extremely useful for cloning arrays and objects. Since both are reference types (and not primitives), copying them safely (i.e. preventing future mutation of the copied original) can be tricky. With the spread operator you have an easy way of creating a (shallow!) clone of the object or array.*/
+
+
+
+// Destructuring
+// Destructuring allows you to easily access the values of arrays or objects and assign them to variables.
+
+// Here's an example for an array:
+
+const array = [1, 2, 3];
+const [a, b] = array;
+console.log(a); // prints 1
+console.log(b); // prints 2
+console.log(array); // prints [1, 2, 3]
+
+// And here for an object:
+const myObj = {
+    name: 'Max',
+    age: 28
+}
+const {name} = myObj;
+console.log(name); // prints 'Max'
+console.log(age); // prints undefined
+console.log(myObj); // prints {name: 'Max', age: 28}
+
+
+// Destructuring is very useful when working with function arguments. Consider this example:
+const printName = (personObj) => {
+    console.log(personObj.name);
+}
+printName({name: 'Max', age: 28}); // prints 'Max'
+
+
+// Here, we only want to print the name in the function but we pass a complete person object to the function.
+// Of course this is no issue but it forces us to call personObj.name inside of our function.
+// We can condense this code with destructuring:
+const printName = ({name}) => {
+    console.log(name);
+}
+printName({name: 'Max', age: 28}); // prints 'Max')
+
+// We get the same result as above but we save some code.
+// By destructuring, we simply pull out the name  property and store it in a variable/ argument named name  which we then can use in the function body.
